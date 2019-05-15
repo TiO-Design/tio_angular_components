42c5363c689fa82d4323ed813894755e4a8262d4
saibotma <tobias.marschall@online.de>
2019-05-15 12:14:06 +0200

    feat/components-popup: Use angular components for popup

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+'use strict';
+const sass = require('node-sass');
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    implementation: sass,
+                    //outputStyle: 'compressed',
+                    sourceMap: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: "lib/",
+                    dest: "lib/",
+                    src: ['**/*.scss'],
+                    ext: '.css'
+                }]
+            }
+        },
+        watch: {
+            options: {
+                livereload: true,
+                reload: true,
+                atBegin: true // Runs every tasks at the beginning (once)
+            },
+            sass: {
+                files: [
+                    '**/*.scss'
+                ],
+                tasks: ['sass', 'postcss']
+            }
+        },
+        postcss: {
+            options: {
+                remove: false,
+                processors: [
+                    require('autoprefixer')({remove: false})
+                ]
+            },
+            dist: {
+                src: '**/*.css'
+            }
+        }
+    });
+
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-postcss');
+
+    grunt.registerTask('dev', [
+        'watch'
+    ]);
+
+};
--- example/lib/app_component.dart
+++ example/lib/app_component.dart
@@ -1,22 +1,16 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
 import 'package:logging/logging.dart';
-
 import 'package:tio_angular_components/tio_angular_components.dart';
-import 'package:tio_angular_components/tio_popup/alignment.dart';
-import 'package:tio_angular_components/tio_popup/tio_overlay_service.dart';
-import 'package:tio_angular_components/tio_popup/tio_popup_source.dart';
 
 @Component(
     selector: 'my-app',
     styleUrls: ['app_component.css'],
     templateUrl: 'app_component.html',
     directives: [TioPopupComponent, PopupSourceDirective],
-    providers: <Object>[overlayProviders],
-    exports: [RelativePosition])
+    providers: <Object>[popupBindings])
 class AppComponent implements OnInit {
   var popup1Visible = false;
-  var popup2Visible = false;
-  var popup3Visible = false;
 
   @override
   void ngOnInit() {
